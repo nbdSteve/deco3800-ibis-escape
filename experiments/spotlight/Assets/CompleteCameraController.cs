086de1d4f2e16aa3a25875f54ed490b71183bcd4
shamo98 <42230420+shamo98@users.noreply.github.com>
2019-05-11 15:35:08 +1000

    Spotlight stuff

---
+++ experiments/spotlight/Assets/CompleteCameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompleteCameraController : MonoBehaviour
+{
+    public float speed = 1.5f;
+
+ void Update ()
+ {
+     if (Input.GetKey(KeyCode.LeftArrow))
+     {
+         transform.position += Vector3.left * speed * Time.deltaTime;
+     }
+     if (Input.GetKey(KeyCode.RightArrow))
+     {
+         transform.position += Vector3.right * speed * Time.deltaTime;
+     }
+     if (Input.GetKey(KeyCode.UpArrow))
+     {
+         transform.position += Vector3.up * speed * Time.deltaTime;
+     }
+     if (Input.GetKey(KeyCode.DownArrow))
+     {
+         transform.position += Vector3.down * speed * Time.deltaTime;
+     }
+     
+     if (Input.GetKey(KeyCode.W)){
+     transform.position += Vector3.up * speed * Time.deltaTime;
+     
+     }
+
+}
+}
