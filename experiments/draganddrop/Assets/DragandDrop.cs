55f3e58f0745429f601f2e3ee78e3e16c654d765
shamo98 <42230420+shamo98@users.noreply.github.com>
2019-05-14 07:24:30 +1000

    Add files via upload

---
+++ experiments/draganddrop/Assets/DragandDrop.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+ using UnityEngine.UI;
+ using UnityEngine.EventSystems;
+ using System.Collections;
+ 
+ public class DragandDrop : MonoBehaviour{
+ 
+    float distance = 10;
+    void OnMouseDrag(){
+        Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+        transform.position = objPosition;
+    }
+ }
+
