d41accd38de7e100f0ea224ab02f3c4df83ea42a
Fouad Khalaf <faoodxman@outlook.com>
2019-05-14 01:49:37 +1000

    Wrote a collision script, does not work. Will debug later today.

---
+++ experiments/item_pickup/Assets/OnCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Player")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
